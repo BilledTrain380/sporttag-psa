a6ac26a446944e980d252aba8405532815b169ce
Nicolas Märchy <nicolas.maerchy@me.com>
2017-06-13 17:33:35 +0200

    Change type of SportRepository to nullable.

--- src/main/kotlin/ch/schulealtendorf/sporttagpsa/repository/SportRepository.kt
+++ src/main/kotlin/ch/schulealtendorf/sporttagpsa/repository/SportRepository.kt
@@ -43,4 +43,4 @@ import org.springframework.data.repository.CrudRepository
  * @author nmaerchy
  * @version 0.0.1
  */
-interface SportRepository: CrudRepository<SportEntity, Int?>
+interface SportRepository: CrudRepository<SportEntity?, Int?>
