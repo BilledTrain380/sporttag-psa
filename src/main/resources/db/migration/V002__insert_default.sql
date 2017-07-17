88ab4d3e9d227c19a14da7de51c17ed7d0db5c16
Nicolas Märchy <nicolas.maerchy@me.com>
2017-07-17 17:43:20 +0200

    Add units and disciplines to the database

--- src/main/resources/db/migration/V002__insert_default.sql
+++ src/main/resources/db/migration/V002__insert_default.sql
@@ -2,4 +2,16 @@ INSERT INTO SPORT (name) VALUES
 ('Schatzsuche'),
 ('Mehrkampf'),
 ('Brennball'),
-('Velo- Rollerblades')
+('Velo- Rollerblades');
+
+INSERT INTO UNIT (unit) VALUES
+('Meter'),
+('Sekunden'),
+('Punkte');
+
+INSERT INTO DISCIPLINE (name, UNIT_id) VALUES 
+  ('Schnelllauf', (SELECT id FROM UNIT WHERE unit = 'Sekunden')),
+  (' Weitsprung', (SELECT id FROM UNIT WHERE unit = 'Meter')),
+  ('Weitwurf', (SELECT id FROM UNIT WHERE unit = 'Punkte')),
+  ('Zielwurf', (SELECT id FROM UNIT WHERE unit = 'Punkte')),
+  ('Seilspringen', (SELECT id FROM UNIT WHERE unit = 'Punkte'));
