20e39223dba3d1dd9c9d6db30e8d2ede95e3b9cc
Nicolas Märchy <nicolas.maerchy@me.com>
2017-10-31 17:31:27 +0100

    Update sql migrations files
    Add all sports to the database. In addition, the attribute for the result table is renamed
    to result instead of performance, due semantic reasons.

--- src/main/resources/db/migration/V002__insert_default.sql
+++ src/main/resources/db/migration/V002__insert_default.sql
@@ -12,9 +12,10 @@ INSERT INTO UNIT (unit) VALUES
 INSERT INTO DISCIPLINE (name, FK_UNIT_id) VALUES 
   ('Schnelllauf', (SELECT id FROM UNIT WHERE unit = 'Sekunden')),
   ('Weitsprung', (SELECT id FROM UNIT WHERE unit = 'Meter')),
-  ('Weitwurf', (SELECT id FROM UNIT WHERE unit = 'Punkte')),
-  ('Zielwurf', (SELECT id FROM UNIT WHERE unit = 'Punkte')),
-  ('Seilspringen', (SELECT id FROM UNIT WHERE unit = 'Punkte'));
+  ('Ballwurf', (SELECT id FROM UNIT WHERE unit = 'Punkte')),
+  ('Ballzielwurf', (SELECT id FROM UNIT WHERE unit = 'Punkte')),
+  ('Seilspringen', (SELECT id FROM UNIT WHERE unit = 'Punkte')),
+  ('Korbeinwurf', (SELECT id FROM UNIT WHERE unit = 'Punkte'));
 
 INSERT INTO PARTICIPATION (is_finished) VALUES
   (FALSE);
