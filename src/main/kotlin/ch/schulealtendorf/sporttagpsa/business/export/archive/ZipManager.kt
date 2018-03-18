bd6e82a0ed7bc98dfba690ac293da2a278d681a9
Nicolas Märchy <nicolas.maerchy@me.com>
2018-03-18 15:32:04 +0100

    Create export package
    The export package contains a archive manager which creates an zip
    archive by given files. In addiotion a storage supplier was introduces
    to get the OS specific application support directory.

---
+++ src/main/kotlin/ch/schulealtendorf/sporttagpsa/business/export/archive/ZipManager.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2018 by Nicolas Märchy
+ *
+ * This file is part of Sporttag PSA.
+ *
+ * Sporttag PSA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sporttag PSA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sporttag PSA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von Sporttag PSA.
+ *
+ * Sporttag PSA ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * Sporttag PSA wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package ch.schulealtendorf.sporttagpsa.business.export.archive
+
+import ch.schulealtendorf.sporttagpsa.business.storage.StorageSupplier
+import net.lingala.zip4j.core.ZipFile
+import net.lingala.zip4j.model.ZipParameters
+import net.lingala.zip4j.util.Zip4jConstants
+import org.springframework.stereotype.Component
+import java.io.File
+
+/**
+ * Archive manager which manages .zip files.
+ * 
+ * @author nmaerchy
+ * @version 1.0.0
+ */
+@Component
+class ZipManager(
+        private val storageSupplier: StorageSupplier
+): ArchiveManager {
+    
+    private val ZIP_OUTPUT = "Ranglisten.zip"
+    
+    /**
+     * Creates an archive with the given {@code files}.
+     *
+     * @param files files to include in the archive
+     *
+     * @return the generated archive
+     */
+    override fun createArchive(files: Iterable<File>): File {
+        
+        val zipFile = storageSupplier.get().resolve(ZIP_OUTPUT)
+        
+        val rankingZip = ZipFile(zipFile)
+        val parameters = ZipParameters().apply { 
+            compressionMethod = Zip4jConstants.COMP_DEFLATE
+            compressionLevel = Zip4jConstants.DEFLATE_LEVEL_NORMAL
+        }
+        
+        files.forEach { rankingZip.addFile(it, parameters) }
+        
+        return zipFile
+    }
+}
