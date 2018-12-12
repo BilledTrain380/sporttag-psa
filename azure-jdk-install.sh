ac58a186c05f8050ccdf72baf29b052154c9db91
BilledTrain380 <billedtrain380@gmail.com>
2018-12-12 13:59:49 +0100

    Fix wget command

--- azure-jdk-install.sh
+++ azure-jdk-install.sh
@@ -5,8 +5,6 @@
 url="https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz"
 
 # Oracle prevents a download without license agreement with a cookie. Workaround: https://stackoverflow.com/questions/10268583/downloading-java-jdk-on-linux-via-wget-is-shown-license-page-instead
-header="Cookie: gpw_e24=http%3a%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie;"
-
 echo "OpenJDK download started..."
-wget -P $AGENT_TOOLSDIRECTORY --no-cookies --no-check-certificate --header $header $url
+wget -P $AGENT_TOOLSDIRECTORY --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3a%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie;" $url
 echo "OpenJDK download finished!"
