b22251dd45ecae7b1cb243046124d4fb004c0ae9
BilledTrain380 <billedtrain380@gmail.com>
2019-04-04 23:24:07 +0200

    Add shared module classes

---
+++ shared/src/main/kotlin/ch/schulealtendorf/psa/shared/reporting/rulebook/CategoryRuleBook.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019 by Nicolas Märchy
+ *
+ * This file is part of Sporttag PSA.
+ *
+ * Sporttag PSA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sporttag PSA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sporttag PSA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von Sporttag PSA.
+ *
+ * Sporttag PSA ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * Sporttag PSA wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package ch.schulealtendorf.psa.shared.reporting.rulebook
+
+/**
+ * Describes a rule book that provides the distance for a specific category.
+ * 
+ * @author nmaerchy
+ * @version 1.0.0
+ */
+interface CategoryRuleBook {
+
+    /**
+     * Defines the category depending on the given 
+     * {@code fact} and then determines the distance.
+     * 
+     * @param fact the fact to use
+     * 
+     * @return the resulting distance or null if no distance applies for the given {@code fact}
+     */
+    fun getDistance(fact: CategoryModel): String?
+}
---
+++ shared/src/main/kotlin/ch/schulealtendorf/psa/shared/reporting/rulebook/FormulaRule.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 by Nicolas Märchy
+ *
+ * This file is part of Sporttag PSA.
+ *
+ * Sporttag PSA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sporttag PSA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sporttag PSA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von Sporttag PSA.
+ *
+ * Sporttag PSA ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * Sporttag PSA wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package ch.schulealtendorf.psa.shared.reporting.rulebook
+
+import ch.schulealtendorf.psa.shared.reporting.rulebook.rules.Rule
+
+/**
+ * @author nmaerchy
+ * @version 0.0.1
+ */
+abstract class FormulaRule: Rule<FormulaModel, Int>() {
+
+    protected abstract val formula: (Double) -> Int
+    
+    /**
+     * @return the result for this rule
+     */
+    override val then: (FormulaModel) -> Int = { formula(it.result) }
+    
+    protected infix fun Double.pow(exponent: Double) = Math.pow(this, exponent)
+    
+    protected fun Boolean.isMale() = this
+    
+    protected fun Boolean.isFemale() = !this
+}
