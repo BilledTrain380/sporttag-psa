8e9ecff51d9e76bca3275337c4a985de1693945c
BilledTrain380 <billedtrain380@gmail.com>
2019-04-08 22:35:37 +0200

    Add participant list reporting

---
+++ shared/src/main/kotlin/ch/schulealtendorf/psa/shared/reporting/participation/JasperParticipantListApi.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2019 by Nicolas Märchy
+ *
+ * This file is part of Sporttag PSA.
+ *
+ * Sporttag PSA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sporttag PSA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sporttag PSA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von Sporttag PSA.
+ *
+ * Sporttag PSA ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * Sporttag PSA wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package ch.schulealtendorf.psa.shared.reporting.participation
+
+import ch.schulealtendorf.psa.core.io.ApplicationFile
+import ch.schulealtendorf.psa.core.io.FileSystem
+import ch.schulealtendorf.psa.dto.ParticipantDto
+import ch.schulealtendorf.psa.dto.SportDto
+import ch.schulealtendorf.psa.shared.reporting.ReportManager
+import ch.schulealtendorf.psa.shared.reporting.Template
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource
+import org.springframework.stereotype.Component
+import java.io.File
+import java.io.InputStream
+
+/**
+ * @author nmaerchy <billedtrain380@gmail.com>
+ * @since 2.1.0
+ */
+@Component
+class JasperParticipantListApi(
+        private val reportManager: ReportManager,
+        private val filesystem: FileSystem
+) : ParticipantListApi {
+
+    override fun createReport(data: Collection<ParticipantDto>, config: SportDto): File {
+
+        val participants = data
+                .map { ParticipantDataSet from it }
+                .sortedBy { it.group }
+
+
+        val parameters: Map<String, Any> = hashMapOf(
+                "sport" to config.name,
+                "participants" to JRBeanCollectionDataSource(participants)
+        )
+
+        val template = object : Template {
+            override val source: InputStream
+                get() = JasperParticipantListApi::class.java.classLoader.getResourceAsStream("/reporting/jasper-templates/participant-list.jrxml")
+            override val parameters = parameters
+        }
+
+        val reportInputStream = reportManager.exportToPdf(template)
+        val file = ApplicationFile("reporting", "Teilnehmerliste.pdf")
+
+        return filesystem.write(file, reportInputStream)
+    }
+}
