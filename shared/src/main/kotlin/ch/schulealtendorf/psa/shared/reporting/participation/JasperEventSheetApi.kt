d0fe200c6b41730e5f2fcce93353ad1ca799a144
BilledTrain380 <billedtrain380@gmail.com>
2019-04-14 11:05:59 +0200

    Add event sheet api

---
+++ shared/src/main/kotlin/ch/schulealtendorf/psa/shared/reporting/participation/JasperEventSheetApi.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2019 by Nicolas Märchy
+ *
+ * This file is part of Sporttag PSA.
+ *
+ * Sporttag PSA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sporttag PSA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sporttag PSA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von Sporttag PSA.
+ *
+ * Sporttag PSA ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * Sporttag PSA wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package ch.schulealtendorf.psa.shared.reporting.participation
+
+import ch.schulealtendorf.psa.core.io.ApplicationFile
+import ch.schulealtendorf.psa.core.io.FileSystem
+import ch.schulealtendorf.psa.dto.CompetitorDto
+import ch.schulealtendorf.psa.dto.GenderDto
+import ch.schulealtendorf.psa.shared.reporting.ReportManager
+import ch.schulealtendorf.psa.shared.reporting.Template
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource
+import org.springframework.stereotype.Component
+import java.io.File
+import java.io.InputStream
+
+/**
+ * @author nmaerchy <billedtrain380@gmail.com>
+ * @since 2.1.0
+ */
+@Component
+class JasperEventSheetApi(
+        private val reportManager: ReportManager,
+        private val filesystem: FileSystem
+) : EventSheetApi {
+    override fun createReport(data: Collection<CompetitorDto>, config: EventSheetConfig): File {
+
+        val competitors: List<EventSheetDataSet> = data.map {
+
+            if (config.hasDistance.not()) {
+                return@map EventSheetDataSet from it
+            }
+
+            val distance = it.results.find { it.discipline.name == config.discipline.name }?.distance ?: ""
+            EventSheetDataSet.from(it, distance)
+        }.sortedBy { it.startnumber }
+
+        val parameters: Map<String, Any> = hashMapOf(
+                "discipline" to config.discipline.name,
+                "gender" to config.gender.name,
+                "group" to config.group.name,
+                "multipleTrials" to config.hasTrials,
+                "withDistance" to config.hasDistance,
+                "competitors" to JRBeanCollectionDataSource(competitors))
+
+
+        val template = object : Template {
+            override val source: InputStream
+                get() = JasperParticipantListApi::class.java.classLoader.getResourceAsStream("/reporting/jasper-templates/event-sheet.jrxml")
+            override val parameters = parameters
+        }
+
+        val reportInputStream = reportManager.exportToPdf(template)
+        val file = ApplicationFile("reporting", "Wettkampfblatt ${config.discipline.name} ${config.group.name} ${config.gender.text()}.pdf")
+
+        return filesystem.write(file, reportInputStream)
+    }
+
+    private fun GenderDto.text(): String {
+        return when (this) {
+            GenderDto.MALE   -> "Knaben"
+            GenderDto.FEMALE -> "Mädchen"
+            else             -> ""
+        }
+    }
+}
