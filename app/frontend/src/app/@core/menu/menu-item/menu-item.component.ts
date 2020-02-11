063336dc50ed2357c8571f028c55d630c60dd66f
BilledTrain380 <billedtrain380@gmail.com>
2020-02-11 21:28:08 +0100

    Add mobile responsive layout

--- app/frontend/src/app/@core/menu/menu-item/menu-item.component.ts
+++ app/frontend/src/app/@core/menu/menu-item/menu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 import { PageMenu } from "../page-menu";
 
@@ -7,16 +7,10 @@ import { PageMenu } from "../page-menu";
   templateUrl: "./menu-item.component.html",
   styleUrls: ["./menu-item.component.scss"],
 })
-export class MenuItemComponent implements OnChanges {
+export class MenuItemComponent {
   @Input()
   readonly item?: PageMenu;
 
   @Input()
   readonly parentId?: string;
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(this.item);
-  }
-
-
 }
--- app/frontend/src/app/@theme/sidebar/sidebar.component.ts
+++ app/frontend/src/app/@theme/sidebar/sidebar.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { faBars, faChevronDown, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 import { PageMenu } from "../../@core/menu/page-menu";
-import { faBars, faChevronDown, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-sidebar",
   templateUrl: "./sidebar.component.html",
   styleUrls: ["./sidebar.component.scss"],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
 
   @Input()
   items: ReadonlyArray<PageMenu> = [];
@@ -28,9 +29,29 @@ export class SidebarComponent {
 
   private _currentExpandedMenuId?: string;
 
-  constructor(readonly route: ActivatedRoute) {
+  private readonly destroy$ = new Subject();
+
+  constructor(
+    readonly route: ActivatedRoute,
+    private readonly breakpointObserver: BreakpointObserver,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([
+        Breakpoints.Tablet,
+        Breakpoints.Small,
+        Breakpoints.XSmall,
+      ])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(result => this.collapsed = result.matches);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   toggleCollapse(item: PageMenu): void {
     this._currentExpandedMenuId = this._currentExpandedMenuId === item.id
