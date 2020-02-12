8572c775633f56606bbc83c3583b7a05e33ce167
BilledTrain380 <billedtrain380@gmail.com>
2020-02-12 19:57:10 +0100

    Add browser info log

--- app/frontend/src/app/app.component.ts
+++ app/frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { NGXLogger } from "ngx-logger";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  readonly title = "frontend";
+export class AppComponent implements OnInit {
+
+  constructor(private readonly log: NGXLogger) {
+  }
+
+  ngOnInit(): void {
+    this.log.info("Started PSA frontend Angular application");
+    this.log.info("Browser version: ", navigator.userAgent);
+  }
 }
