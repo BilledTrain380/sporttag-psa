4015b2b8931802f543df15f40a3f8706a12bf98a
BilledTrain380 <billedtrain380@gmail.com>
2020-02-14 20:00:31 +0100

    Add core testing module

---
+++ app/frontend/src/app/@core/core-testing-module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { LoggerTestingModule } from "ngx-logger/testing";
+
+import { CoreModule } from "./core.module";
+
+const MODULES = [
+  CommonModule,
+  CoreModule,
+  NoopAnimationsModule,
+  FontAwesomeModule,
+  LoggerTestingModule,
+];
+
+@NgModule({
+  imports: [...MODULES],
+  exports: [...MODULES],
+})
+export class CoreTestingModule {
+}
