b6dbe7855952d6211e5d885a13d8bd77c8e2dd95
BilledTrain380 <billedtrain380@gmail.com>
2020-02-16 21:55:21 +0100

    Add e2e testing with lambdatest

---
+++ app/frontend/e2e/protractor.lambdatest.conf.js
@@ -0,0 +1,41 @@
+const {config: baseConfig} = require("./protractor.conf");
+const LambdaTunnel = require("@lambdatest/node-tunnel");
+
+exports.config = {
+  ...baseConfig,
+
+  lambdatestUser: process.env.LT_USERNAME,
+  lambdatestKey: process.env.LT_ACCESS_KEY,
+
+  directConnect: false,
+
+  baseUrl: 'http://localhost:8080/app/',
+
+  seleniumAddress: `https://${process.env.LT_USERNAME}:${process.env.LT_ACCESS_KEY}@hub.lambdatest.com/wd/hub`,
+
+  capabilities: {
+    "build": `PSA-${process.env.FULL_VERSION}`,
+    "name": "PSA e2e",
+    "platform": "MacOS Catalina",
+    "browserName": "Safari",
+    "version": "13.0",
+    "resolution": "1440x900",
+    "tunnel": true
+  },
+
+  beforeLaunch: async () => {
+    const tunnelInstance = new LambdaTunnel();
+
+    const tunnelArgs = {
+      user: process.env.LT_USERNAME,
+      key: process.env.LT_ACCESS_KEY,
+    };
+
+    try {
+      await tunnelInstance.start(tunnelArgs);
+      console.log("LambdaTest Tunnel is running");
+    } catch (e) {
+      console.log("Failed to start LambdaTest Tunnel", e);
+    }
+  }
+};
