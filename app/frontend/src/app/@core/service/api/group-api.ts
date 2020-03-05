ac771de045b67626c40318a1efd36986430e504d
BilledTrain380 <billedtrain380@gmail.com>
2020-03-05 19:29:02 +0100

    Add group api class

---
+++ app/frontend/src/app/@core/service/api/group-api.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable, InjectionToken } from "@angular/core";
+import { NGXLogger } from "ngx-logger";
+import { Observable } from "rxjs";
+
+export const GROUP_API = new InjectionToken("Group Api implementation");
+
+export interface GroupDto {
+  readonly name: string;
+  readonly coach: string;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class GroupApi {
+  constructor(
+    private readonly http: HttpClient,
+    private readonly log: NGXLogger,
+  ) {
+  }
+
+  getGroups(options: {
+    readonly competitive?: boolean;
+    readonly pendingParticipation?: boolean;
+  }): Observable<ReadonlyArray<GroupDto>> {
+    this.log.info(`Get groups: options=${options}`);
+
+    const params = new HttpParams();
+
+    if (options && options.competitive) {
+      params.append("competitive", `${options.competitive}`);
+    }
+
+    if (options && options.pendingParticipation) {
+      params.append("pendingParticipation", `${options.pendingParticipation}`);
+    }
+
+    return this.http.get<ReadonlyArray<GroupDto>>("/groups", {params});
+  }
+
+  getGroup(groupName: string): Observable<GroupDto> {
+    this.log.info(`Get group: name=${groupName}`);
+
+    return this.http.get<GroupDto>(`/group/${groupName}`);
+  }
+}
