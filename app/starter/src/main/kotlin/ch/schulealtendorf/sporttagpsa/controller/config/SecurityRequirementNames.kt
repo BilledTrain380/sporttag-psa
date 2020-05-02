6db75fca62b5b8f9a240dd315b87c948446a439d
BilledTrain380 <billedtrain380@gmail.com>
2020-05-02 17:55:15 +0200

    Add security requirements constants

---
+++ app/starter/src/main/kotlin/ch/schulealtendorf/sporttagpsa/controller/config/SecurityRequirementNames.kt
@@ -0,0 +1,9 @@
+package ch.schulealtendorf.sporttagpsa.controller.config
+
+/**
+ * @author nmaerchy <billedtrain380@gmail.com>
+ * @since 2.2.0
+ */
+object SecurityRequirementNames {
+    const val OAUTH2 = "oauth2"
+}
