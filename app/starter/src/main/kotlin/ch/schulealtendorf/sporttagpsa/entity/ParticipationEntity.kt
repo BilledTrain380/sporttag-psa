beee257c9b88edd830cb8bac4495f673ed9f0d58
BilledTrain380 <billedtrain380@gmail.com>
2020-05-03 12:40:54 +0200

    Fix code styles

--- app/starter/src/main/kotlin/ch/schulealtendorf/sporttagpsa/entity/ParticipationEntity.kt
+++ app/starter/src/main/kotlin/ch/schulealtendorf/sporttagpsa/entity/ParticipationEntity.kt
@@ -41,7 +41,6 @@ import org.jetbrains.annotations.NotNull
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
-import javax.persistence.Transient
 import javax.validation.constraints.Size
 
 /**
