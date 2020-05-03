2c2ecfea8687bdd24a1148bb7515fcb409a4a78c
BilledTrain380 <billedtrain380@gmail.com>
2020-05-03 14:53:17 +0200

    Switch rule set test to junit

---
+++ app/shared/src/test/kotlin/ch/schulealtendorf/psa/shared/rulebook/BroadJumpRuleSetTest.kt
@@ -0,0 +1,29 @@
+package ch.schulealtendorf.psa.shared.rulebook
+
+import ch.schulealtendorf.psa.dto.participation.GenderDto
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("unit-test")
+internal class BroadJumpRuleSetTest {
+    private val ruleSet = BroadJumpRuleSet()
+
+    @Test
+    internal fun onFemaleCompetitors() {
+        val model = FormulaModel("Weitsprung", null, 4.31, GenderDto.FEMALE)
+
+        val points: Int = ruleSet.getRules().first { it.whenever(model) }.then(model)
+
+        assertThat(points).isEqualTo(553)
+    }
+
+    @Test
+    internal fun onMaleCompetitors() {
+        val model = FormulaModel("Weitsprung", null, 3.32, GenderDto.MALE)
+
+        val points: Int = ruleSet.getRules().first { it.whenever(model) }.then(model)
+
+        assertThat(points).isEqualTo(256)
+    }
+}
---
+++ app/shared/src/test/kotlin/ch/schulealtendorf/psa/shared/rulebook/SprintRuleSetTest.kt
@@ -0,0 +1,29 @@
+package ch.schulealtendorf.psa.shared.rulebook
+
+import ch.schulealtendorf.psa.dto.participation.GenderDto
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("unit-test")
+internal class SprintRuleSetTest {
+    private val ruleSet = SprintRuleSet()
+
+    @Test
+    internal fun onFemaleCompetitors() {
+        val model = FormulaModel("Schnelllauf", "60m", 10.99, GenderDto.FEMALE)
+
+        val points: Int = ruleSet.getRules().first { it.whenever(model) }.then(model)
+
+        assertThat(points).isEqualTo(224)
+    }
+
+    @Test
+    internal fun onMaleCompetitors() {
+        val model = FormulaModel("Schnelllauf", "60m", 11.4, GenderDto.MALE)
+
+        val points: Int = ruleSet.getRules().first { it.whenever(model) }.then(model)
+
+        assertThat(points).isEqualTo(128)
+    }
+}
