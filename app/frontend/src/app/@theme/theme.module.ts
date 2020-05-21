9137c50506603aafeef162d3506b868b6f2cfb50
BilledTrain380 <billedtrain380@gmail.com>
2020-05-21 13:31:00 +0200

    Recover card component

--- app/frontend/src/app/@theme/theme.module.ts
+++ app/frontend/src/app/@theme/theme.module.ts
@@ -8,6 +8,7 @@ import { CoreModule } from "../@core/core.module";
 
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
+import { CardComponent } from "./layout/card/card.component";
 import { ColumnLayoutComponent } from "./layout/column-layout/column-layout.component";
 import { LayoutButtonComponent } from "./layout/layout-button/layout-button.component";
 import { LayoutButtonsComponent } from "./layout/layout-buttons/layout-buttons.component";
@@ -21,6 +22,7 @@ const BASE_COMPONENTS = [
   HeaderComponent,
   FooterComponent,
   SidebarComponent,
+  CardComponent,
   SimplePageComponent,
   RowPageComponent,
   RowLayoutComponent,
