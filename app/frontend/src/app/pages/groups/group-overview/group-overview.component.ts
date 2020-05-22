b9fc6fb7dd41b733c1d6f83f1d3a09e12af9a9b9
BilledTrain380 <billedtrain380@gmail.com>
2020-05-22 14:01:09 +0200

    Implement group import

--- app/frontend/src/app/pages/groups/group-overview/group-overview.component.ts
+++ app/frontend/src/app/pages/groups/group-overview/group-overview.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 
 import { OverviewGroupDto } from "../../../dto/group";
+import { Alert } from "../../../modules/alert/alert";
 import { AppState } from "../../../store/app";
 import { loadGroupsOverviewAction } from "../../../store/group/group.action";
 import { selectGroups } from "../../../store/group/group.selector";
+
+import { ImportGroupsComponent } from "./import-groups/import-groups.component";
 
 @Component({
              selector: "app-group-overview",
@@ -18,12 +22,31 @@ export class GroupOverviewComponent implements OnInit {
 
   readonly faUpload = faUpload;
 
+  alert?: Alert;
+
   constructor(
     private readonly store: Store<AppState>,
+    private readonly modalService: NgbModal,
   ) {
   }
 
   ngOnInit(): void {
+    this.loadGroups();
+  }
+
+  openImportModal(): void {
+    this.modalService
+      .open(ImportGroupsComponent, {size: "lg"})
+      .result
+      .then((alert?: Alert) => {
+        if (alert?.isSuccess()) {
+          this.alert = alert;
+          this.loadGroups();
+        }
+      });
+  }
+
+  private loadGroups(): void {
     this.store.dispatch(loadGroupsOverviewAction({}));
   }
 }
--- app/frontend/src/app/pages/groups/groups.module.ts
+++ app/frontend/src/app/pages/groups/groups.module.ts
@@ -2,14 +2,26 @@ import { NgModule } from "@angular/core";
 import { EffectsModule } from "@ngrx/effects";
 
 import { ThemeModule } from "../../@theme/theme.module";
+import { AlertModule } from "../../modules/alert/alert.module";
+import { DragAndDropModule } from "../../modules/drag-and-drop/drag-and-drop.module";
+import { ModalModule } from "../../modules/modal/modal.module";
+import { StatusModule } from "../../modules/status/status.module";
 import { GroupEffects } from "../../store/group/group.effect";
 
 import { GroupOverviewComponent } from "./group-overview/group-overview.component";
+import { ImportGroupsComponent } from "./group-overview/import-groups/import-groups.component";
 
 @NgModule({
-            declarations: [GroupOverviewComponent],
+            declarations: [
+              GroupOverviewComponent,
+              ImportGroupsComponent,
+            ],
             imports: [
               ThemeModule,
+              StatusModule,
+              AlertModule,
+              ModalModule,
+              DragAndDropModule,
               EffectsModule.forFeature([GroupEffects]),
             ],
           })
