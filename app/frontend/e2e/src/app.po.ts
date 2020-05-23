51af0f2a8f39a4f2c2c0aa8a7daa2532d05c81e6
BilledTrain380 <billedtrain380@gmail.com>
2020-05-23 18:05:38 +0200

    Remove unnecessary e2e tests

--- app/frontend/e2e/src/app.po.ts
+++ app/frontend/e2e/src/app.po.ts
@@ -1,16 +1,10 @@
-import { browser, by, element, ElementFinder } from "protractor";
+import { browser, by, element } from "protractor";
 
 export class AppPage {
+  readonly username = element(by.css('[data-test-selector="user-menu"]'))
+    .getText() as Promise<string>;
+
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
-  }
-
-  getUsername(): Promise<string> {
-    return element(by.css('[data-test-selector="user-menu"]'))
-      .getText() as Promise<string>;
-  }
-
-  getMenu(): ElementFinder {
-    return element(by.css('app-sidebar [data-test-selector="navigation-menu"]'));
   }
 }
