cc7b13e87e645e46c69846326fb232bee24e29ea
BilledTrain380 <billedtrain380@gmail.com>
2020-05-23 17:11:20 +0200

    Add administration overview page

--- app/frontend/src/app/pages/administration/administration-routing.module.ts
+++ app/frontend/src/app/pages/administration/administration-routing.module.ts
@@ -3,17 +3,29 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AdministrationComponent } from "./administration.component";
 import { GroupOverviewComponent } from "./groups/group-overview/group-overview.component";
+import { OverviewComponent } from "./overview/overview.component";
 
-const routes: Routes = [{
-  path: "",
-  component: AdministrationComponent,
-  children: [
-    {
-      path: "group-manager",
-      component: GroupOverviewComponent,
-    },
-  ],
-}];
+const routes: Routes = [
+  {
+    path: "",
+    component: AdministrationComponent,
+    children: [
+      {
+        path: "overview",
+        component: OverviewComponent,
+      },
+      {
+        path: "group-management",
+        component: GroupOverviewComponent,
+      },
+      {
+        path: "",
+        redirectTo: "overview",
+        pathMatch: "full",
+      },
+    ],
+  },
+];
 
 @NgModule({
             imports: [RouterModule.forChild(routes)],
--- app/frontend/src/app/pages/administration/administration.module.ts
+++ app/frontend/src/app/pages/administration/administration.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 import { ThemeModule } from "../../@theme/theme.module";
@@ -6,14 +5,18 @@ import { ThemeModule } from "../../@theme/theme.module";
 import { AdministrationRoutingModule } from "./administration-routing.module";
 import { AdministrationComponent } from "./administration.component";
 import { GroupsModule } from "./groups/groups.module";
+import { OverviewComponent } from "./overview/overview.component";
 
 @NgModule({
-  declarations: [AdministrationComponent],
-  imports: [
-    CommonModule,
-    ThemeModule,
-    AdministrationRoutingModule,
-    GroupsModule,
-  ],
-})
-export class AdministrationModule { }
+            declarations: [
+              AdministrationComponent,
+              OverviewComponent,
+            ],
+            imports: [
+              ThemeModule,
+              AdministrationRoutingModule,
+              GroupsModule,
+            ],
+          })
+export class AdministrationModule {
+}
