2fccb2c064c1a23c0b98513b9ad7af0f1b51fb75
BilledTrain380 <billedtrain380@gmail.com>
2020-06-07 17:22:37 +0200

    Fix setup controller test

--- app/starter/src/test/kotlin/ch/schulealtendorf/sporttagpsa/controller/authorization/SetupControllerTest.kt
+++ app/starter/src/test/kotlin/ch/schulealtendorf/sporttagpsa/controller/authorization/SetupControllerTest.kt
@@ -1,23 +1,34 @@
 package ch.schulealtendorf.sporttagpsa.controller.authorization
 
-import ch.schulealtendorf.sporttagpsa.controller.PsaWebMvcTest
+import ch.schulealtendorf.sporttagpsa.controller.formContent
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.model
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+@ActiveProfiles("test")
+@Tag("ctrl-test")
 @SpringBootTest(properties = ["spring.flyway.locations=classpath:/db/migration"])
+@AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-internal class SetupControllerTest : PsaWebMvcTest() {
+internal class SetupControllerTest {
     companion object {
         private const val SETUP_PATH = "/setup"
     }
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
     @Test
     internal fun getSetupPage() {
