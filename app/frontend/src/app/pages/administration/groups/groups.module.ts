8e77a2509ef41081b6fd565d88219ddc16dfab55
BilledTrain380 <billedtrain380@gmail.com>
2020-06-12 23:24:16 +0200

    Introduce routing module to group management

--- app/frontend/src/app/pages/administration/groups/groups.module.ts
+++ app/frontend/src/app/pages/administration/groups/groups.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { EffectsModule } from "@ngrx/effects";
 
 import { ThemeModule } from "../../../@theme/theme.module";
+import { ActionButtonsModule } from "../../../modules/action-buttons/action-buttons.module";
 import { AlertModule } from "../../../modules/alert/alert.module";
 import { DragAndDropModule } from "../../../modules/drag-and-drop/drag-and-drop.module";
 import { ModalModule } from "../../../modules/modal/modal.module";
@@ -10,9 +11,12 @@ import { GroupEffects } from "../../../store/group/group.effect";
 
 import { GroupOverviewComponent } from "./group-overview/group-overview.component";
 import { ImportGroupsComponent } from "./group-overview/import-groups/import-groups.component";
+import { GroupsRoutingModule } from "./groups-routing.module";
+import { GroupsComponent } from "./groups.component";
 
 @NgModule({
             declarations: [
+              GroupsComponent,
               GroupOverviewComponent,
               ImportGroupsComponent,
             ],
@@ -20,7 +24,9 @@ import { ImportGroupsComponent } from "./group-overview/import-groups/import-gro
               ThemeModule,
               StatusModule,
               AlertModule,
+              ActionButtonsModule,
               ModalModule,
+              GroupsRoutingModule,
               DragAndDropModule,
               EffectsModule.forFeature([GroupEffects]),
             ],
