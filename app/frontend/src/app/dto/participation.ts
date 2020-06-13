f2c94a322be201619e4f1828d9e3b3bebd4e24ab
BilledTrain380 <billedtrain380@gmail.com>
2020-06-13 21:49:16 +0200

    Add participant builder

--- app/frontend/src/app/dto/participation.ts
+++ app/frontend/src/app/dto/participation.ts
@@ -1,3 +1,4 @@
+import { requireNonNullOrUndefined } from "../@core/lib/lib";
 import { SimpleGroupDto } from "./group";
 import { StatusDto } from "./status";
 
@@ -17,6 +18,116 @@ export interface ParticipantDto {
   readonly town: TownDto;
   readonly group: SimpleGroupDto;
   readonly sportType?: string;
+}
+
+export class ParticipantDtoBuilder {
+  private id?: number;
+  private surname?: string;
+  private prename?: string;
+  private gender?: GenderDto;
+  private birthday?: BirthdayDto;
+  private isAbsent?: boolean;
+  private address?: string;
+  private town?: TownDto;
+  private group?: SimpleGroupDto;
+  private sportType?: string;
+
+  private constructor() {
+  }
+
+  static newBuilder(dto?: ParticipantDto): ParticipantDtoBuilder {
+    const builder = new ParticipantDtoBuilder();
+
+    if (dto) {
+      builder.copy(dto);
+    }
+
+    return builder;
+  }
+
+  copy(dto: ParticipantDto): ParticipantDtoBuilder {
+    this.id = dto.id;
+    this.surname = dto.surname;
+    this.prename = dto.prename;
+    this.gender = dto.gender;
+    this.birthday = dto.birthday;
+    this.isAbsent = dto.isAbsent;
+    this.address = dto.address;
+    this.town = dto.town;
+    this.group = dto.group;
+    this.sportType = dto.sportType;
+
+    return this;
+  }
+
+  setId(id: number): ParticipantDtoBuilder {
+    this.id = id;
+
+    return this;
+  }
+
+  setSurname(surname: string): ParticipantDtoBuilder {
+    this.surname = surname;
+
+    return this;
+  }
+
+  setPrename(prename: string): ParticipantDtoBuilder {
+    this.prename = prename;
+
+    return this;
+  }
+
+  setGender(gender: GenderDto): ParticipantDtoBuilder {
+    this.gender = gender;
+
+    return this;
+  }
+
+  setBirthday(birthday: BirthdayDto): ParticipantDtoBuilder {
+    this.birthday = birthday;
+
+    return this;
+  }
+
+  setAbsent(isAbsent: boolean): ParticipantDtoBuilder {
+    this.isAbsent = isAbsent;
+
+    return this;
+  }
+
+  setAddress(address: string): ParticipantDtoBuilder {
+    this.address = address;
+
+    return this;
+  }
+
+  setTown(town: TownDto): ParticipantDtoBuilder {
+    this.town = town;
+
+    return this;
+  }
+
+  setSportType(sportType: string): ParticipantDtoBuilder {
+    this.sportType = sportType;
+
+    return this;
+  }
+
+  build(): ParticipantDto {
+    return {
+      id: requireNonNullOrUndefined(this.id),
+      surname: requireNonNullOrUndefined(this.surname),
+      prename: requireNonNullOrUndefined(this.prename),
+      gender: requireNonNullOrUndefined(this.gender),
+      birthday: requireNonNullOrUndefined(this.birthday),
+      isAbsent: requireNonNullOrUndefined(this.isAbsent),
+      address: requireNonNullOrUndefined(this.address),
+      town: requireNonNullOrUndefined(this.town),
+      group: requireNonNullOrUndefined(this.group),
+      sportType: this.sportType,
+    };
+  }
 }
 
 export enum ParticipationCommand {
@@ -72,7 +183,7 @@ export function translateGender(gender: GenderDto): string {
 }
 
 export interface BirthdayDto {
-  readonly time: number;
+  readonly value: Date;
 }
 
 export interface TownDto {
