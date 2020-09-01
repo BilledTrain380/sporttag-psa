9e04e540e977ab92d5ea99d73ad639873989d7ce
BilledTrain380 <billedtrain380@gmail.com>
2020-09-01 15:30:00 +0200

    Implement participation management page

--- app/frontend/src/app/pages/administration/participation/participation-management/participation-management.component.ts
+++ app/frontend/src/app/pages/administration/participation/participation-management/participation-management.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from "@angular/core";
+import { faEraser } from "@fortawesome/free-solid-svg-icons/faEraser";
+import { faLock } from "@fortawesome/free-solid-svg-icons/faLock";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+
+import { ParticipationCommand, ParticipationDto } from "../../../../dto/participation";
+import { confirmModalOptions, ConfirmType } from "../../../../modules/modal/confirm-modal/confirm-modal-util";
+import { ConfirmModalComponent } from "../../../../modules/modal/confirm-modal/confirm-modal.component";
+import { loadParticipationStatusAction, updateParticipationStatusAction } from "../../../../store/participation/participation.action";
+import { selectParticipationStatus } from "../../../../store/participation/participation.selector";
+import { VOID_PROPS } from "../../../../store/standard-props";
 
 @Component({
              selector: "app-participation-management",
@@ -6,8 +18,48 @@ import { Component, OnInit } from "@angular/core";
              styleUrls: ["./participation-management.component.scss"],
            })
 export class ParticipationManagementComponent implements OnInit {
+  readonly faLock = faLock;
+  readonly faEraser = faEraser;
 
-  ngOnInit(): void {
+  participation$: Observable<ParticipationDto> = this.store.select(selectParticipationStatus);
+
+  constructor(
+    private readonly store: Store,
+    private readonly modalService: NgbModal,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.store.dispatch(loadParticipationStatusAction(VOID_PROPS));
+  }
+
+  closeParticipation(): void {
+    const modalRef = this.modalService.open(ConfirmModalComponent, confirmModalOptions);
+
+    modalRef.componentInstance.buildText(
+      $localize`Do you really want to close the participation?`,
+    );
+
+    modalRef.result
+      .then((type: ConfirmType) => {
+        if (type === ConfirmType.CONFIRM) {
+          this.store.dispatch(updateParticipationStatusAction({command: ParticipationCommand.CLOSE}));
+        }
+      });
+  }
+
+  resetParticipation(): void {
+    const modalRef = this.modalService.open(ConfirmModalComponent, confirmModalOptions);
+
+    modalRef.componentInstance.buildText(
+      $localize`Do you really want to reset the participation?`,
+    );
+
+    modalRef.result
+      .then((type: ConfirmType) => {
+        if (type === ConfirmType.CONFIRM) {
+          this.store.dispatch(updateParticipationStatusAction({command: ParticipationCommand.RESET}));
+        }
+      });
+  }
 }
--- app/frontend/src/app/pages/administration/participation/participation.module.ts
+++ app/frontend/src/app/pages/administration/participation/participation.module.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { EffectsModule } from "@ngrx/effects";
+
+import { ThemeModule } from "../../../@theme/theme.module";
+import { ModalModule } from "../../../modules/modal/modal.module";
+import { StatusModule } from "../../../modules/status/status.module";
+import { ParticipationEffects } from "../../../store/participation/participation.effect";
 
 import { ParticipationManagementComponent } from "./participation-management/participation-management.component";
 import { ParticipationRoutingModule } from "./participation-routing.module";
@@ -9,6 +15,10 @@ import { ParticipationRoutingModule } from "./participation-routing.module";
             imports: [
               CommonModule,
               ParticipationRoutingModule,
+              ThemeModule,
+              StatusModule,
+              ModalModule,
+              EffectsModule.forFeature([ParticipationEffects]),
             ],
           })
 export class ParticipationModule {
