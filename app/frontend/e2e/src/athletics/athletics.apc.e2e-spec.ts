3132cc6beb9f8cdd33930da072006cca8c663e4f
BilledTrain380 <billedtrain380@gmail.com>
2020-09-08 15:44:04 +0200

    Fix e2e tests

--- app/frontend/e2e/src/athletics/athletics.apc.e2e-spec.ts
+++ app/frontend/e2e/src/athletics/athletics.apc.e2e-spec.ts
@@ -17,32 +17,10 @@ describe("AthleticsPage", () => {
     await appPage.navigateToAthletics();
   });
 
-  it("should update the points when updating a result", async () => {
-    const resultInput = page.tableRows.first()
-      .element(by.css('[data-test-selector="result-input"]'));
-
-    /* Send a back space and the actual value at once.
-     * The .clear() method does not work here, because due the focus out,
-     * the previous value will be inserted again.
-     */
-    await resultInput.sendKeys(`${protractor.Key.BACK_SPACE}11.25`);
-
-    // Trigger focus out by clicking into another input
-    await page.tableRows.get(1)
-      .element(by.css('[data-test-selector="result-input"]'))
-      .click();
-
-    const points = page.tableRows.get(0)
-      .element(by.cssContainingText('[data-test-selector="result-points"]', "187"));
-
-    await browser.wait(EC.visibilityOf(points));
-
-    await expect(points.getText())
-      .toBe("187");
-  });
-
   it("should switch group", async () => {
     const firstRow = page.tableRows.first();
+
+    await browser.wait(EC.visibilityOf(firstRow));
 
     await page.groupNextButton.click();
 
@@ -57,8 +35,36 @@ describe("AthleticsPage", () => {
       .toBe("Mason");
   });
 
+  it("should update the points when updating a result", async () => {
+    const resultInput = page.tableRows.first()
+      .element(by.css('[data-test-selector="result-input"]'));
+
+    await browser.wait(EC.visibilityOf(resultInput));
+
+    /* Send a back space and the actual value at once.
+     * The .clear() method does not work here, because due the focus out,
+     * the previous value will be inserted again.
+     */
+    await resultInput.sendKeys(`${protractor.Key.BACK_SPACE}11.25`);
+
+    // Trigger focus out by clicking into another input
+    await page.tableRows.get(1)
+      .element(by.css('[data-test-selector="result-input"]'))
+      .click();
+
+    const points = page.tableRows.get(0)
+      .element(by.cssContainingText('[data-test-selector="result-points"]', "144"));
+
+    await browser.wait(EC.visibilityOf(points));
+
+    await expect(points.getText())
+      .toBe("144");
+  });
+
   it("should switch discipline", async () => {
     const firstRow = page.tableRows.first();
+
+    await browser.wait(EC.visibilityOf(firstRow));
 
     const firstRowUnitValue = firstRow.element(by.css("td .result-input-container .input-group-append"));
     const unitValue = await firstRowUnitValue.getText();
