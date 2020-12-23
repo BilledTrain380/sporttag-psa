8993fce8fe6d363f407615b691fa0ec7e6d192d6
BilledTrain380 <billedtrain380@gmail.com>
2020-12-23 16:21:42 +0100

    Remove index page of PSA web
    This page no longer makes sense, therefore it has been removed.

--- app/psa-web/src/test/kotlin/ch/schulealtendorf/psa/web/HomeControllerTest.kt
+++ app/psa-web/src/test/kotlin/ch/schulealtendorf/psa/web/HomeControllerTest.kt
@@ -3,7 +3,6 @@ package ch.schulealtendorf.psa.web
 import ch.schulealtendorf.psa.configuration.test.PsaWebMvcTest
 import org.junit.jupiter.api.Test
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.model
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 internal class HomeControllerTest : PsaWebMvcTest() {
@@ -11,17 +10,6 @@ internal class HomeControllerTest : PsaWebMvcTest() {
     @Test
     internal fun getHomepageWhenUnauthorized() {
         mockMvc.perform(get("/"))
-            .andExpect(status().isOk)
-            .andExpect(model().attribute("isAuthenticated", false))
-    }
-
-    @Test
-    internal fun getHomepageWhenAuthorized() {
-        mockMvc.perform(
-            get("/")
-                .with(user(ADMIN_USER))
-        ).andExpect(status().isOk)
-            .andExpect(model().attribute("isAuthenticated", true))
-            .andExpect(model().attribute("username", ADMIN_USER))
+            .andExpect(status().is3xxRedirection)
     }
 }
